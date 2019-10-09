d5cf56f1c01825b6e56fbca0c734816c62ba3664
wei chen <weichen9083@gmail.com>
2019-10-09 23:52:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # Add your code here
+
+require "pry"
+class Dog 
+  
+  attr_accessor:name 
+  
+  @@all=[]
+  
+  def initialize (name)
+    @name = name
+    save 
+  end 
+  
+  def self.all 
+    @@all
+  end
+
+def save 
+  @@all << self
+
+end 
+
+def self.print_all 
+  @@all.each do |name|
+    puts name.name
+  end 
+end
+
+def self.clear_all 
+  @@all.clear
+  end
+    
+end 
