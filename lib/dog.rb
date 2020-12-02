0e64c366c5903559ea3347b05a3306d47f813a6c
wei chen <weichen9083@gmail.com>
2020-12-02 03:01:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,29 @@
 # Add your code here
 
-require "pry"
+
 class Dog 
   
-  attr_accessor:name 
+  attr_accessor :name 
   
-  @@all=[]
-  
-  def initialize (name)
-    @name = name
-    save 
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self 
   end 
   
   def self.all 
     @@all
-  end
-
-def save 
-  @@all << self
-
-end 
-
-def self.print_all 
-  @@all.each do |name|
-    puts name.name
   end 
-end
-
-def self.clear_all 
-  @@all.clear
-  end
-    
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.print_all
+    @@all.each do |name|
+      puts name.name 
+    end 
+  end 
+  
+  
 end 
